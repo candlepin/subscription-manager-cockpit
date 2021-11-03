marusak/subscription-manager

cockpit: Use current PF4 components and design

cockpit/src/index.js
@@ -24,12 +24,12 @@ import ReactDOM from 'react-dom';
 import '../lib/patternfly/patternfly-cockpit.scss';
 
 import subscriptionsClient from './subscriptions-client';
-import subscriptionsRegister from './subscriptions-register.jsx';
-import subscriptionsView from './subscriptions-view.jsx';
+import SubscriptionsRegister from './subscriptions-register.jsx';
+import SubscriptionsView from './subscriptions-view.jsx';
 import * as Insights from './insights.jsx';
 import * as Dialog from '../lib/cockpit-components-dialog.jsx';
 
-import './subscriptions.css';
+import './subscriptions.scss';
 
 let _ = cockpit.gettext;
 
@@ -106,7 +106,7 @@ function openRegisterDialog() {
                 let dialogProps = {
                     'id': 'register_dialog',
                     'title': _("Register System"),
-                    'body': React.createElement(subscriptionsRegister.dialogBody, registerDialogDetails),
+                    'body': React.createElement(SubscriptionsRegister, registerDialogDetails),
                 };
 
                 if (renderDialog)
@@ -132,7 +132,7 @@ function initStore(rootElement) {
 
     dataStore.render = () => {
         ReactDOM.render(React.createElement(
-            subscriptionsView.page,
+            SubscriptionsView,
             {
                 status: subscriptionsClient.subscriptionStatus.status,
                 status_msg: subscriptionsClient.subscriptionStatus.status_msg,

cockpit/src/subscriptions-register.jsx
@@ -32,7 +32,7 @@ import * as Insights from './insights.jsx';
  *   - onChange  callback to signal when the data has changed
  *   - properties as in defaultRegisterDialogSettings()
  */
-class PatternDialogBody extends React.Component {
+class SubscriptionsRegister extends React.Component {
     render() {
         let customURL;
         if (this.props.url === 'custom') {
@@ -201,6 +201,4 @@ class PatternDialogBody extends React.Component {
     }
 }
 
-module.exports = {
-    dialogBody: PatternDialogBody,
-};
+export default SubscriptionsRegister;

candlepin/subscription-manager-cockpit

(cherry picked from commit ccb325c28b27dc5d01f1820788d4ab5528452beb)

src/index.js
@@ -24,12 +24,12 @@ import ReactDOM from 'react-dom';
 import '../lib/patternfly/patternfly-cockpit.scss';
 
 import subscriptionsClient from './subscriptions-client';
-import subscriptionsRegister from './subscriptions-register.jsx';
-import subscriptionsView from './subscriptions-view.jsx';
+import SubscriptionRegisterDialog from './subscriptions-register.jsx';
+import SubscriptionsView from './subscriptions-view.jsx';
 import * as Insights from './insights.jsx';
 import * as Dialog from '../lib/cockpit-components-dialog.jsx';
 
-import './subscriptions.css';
+import './subscriptions.scss';
 
 let _ = cockpit.gettext;
 
@@ -106,7 +106,7 @@ function openRegisterDialog() {
                 let dialogProps = {
                     'id': 'register_dialog',
                     'title': _("Register System"),
-                    'body': React.createElement(subscriptionsRegister.dialogBody, registerDialogDetails),
+                    'body': React.createElement(SubscriptionRegisterDialog, registerDialogDetails),
                 };
 
                 if (renderDialog)
@@ -132,7 +132,7 @@ function initStore(rootElement) {
 
     dataStore.render = () => {
         ReactDOM.render(React.createElement(
-            subscriptionsView.page,
+            SubscriptionsView,
             {
                 status: subscriptionsClient.subscriptionStatus.status,
                 status_msg: subscriptionsClient.subscriptionStatus.status_msg,

src/subscriptions-register.jsx
@@ -32,7 +32,7 @@ import * as Insights from './insights.jsx';
  *   - onChange  callback to signal when the data has changed
  *   - properties as in defaultRegisterDialogSettings()
  */
-class PatternDialogBody extends React.Component {
+class SubscriptionRegisterDialog extends React.Component {
     render() {
         let customURL;
         if (this.props.url === 'custom') {
@@ -201,6 +201,4 @@ class PatternDialogBody extends React.Component {
     }
 }
 
-module.exports = {
-    dialogBody: PatternDialogBody,
-};
+export default SubscriptionRegisterDialog;